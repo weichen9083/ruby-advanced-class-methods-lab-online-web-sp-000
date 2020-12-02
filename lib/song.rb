2dbb8c8ac3bba01803f32dc89466b74f9dab6fcf
wei chen <weichen9083@gmail.com>
2020-12-02 04:09:55 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,7 +30,12 @@
       
       def self.find_by_name(name)
        self.all.find {|x| x.name == name}
-        
       end 
+      
+      def self.find_or_create_by_name(name)
+        if self.find_by_name(name)
+        else 
+          self.create_by_name(name)
+        end 
      
     end
