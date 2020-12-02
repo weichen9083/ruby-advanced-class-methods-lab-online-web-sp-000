bbe9fc1ff71a82fbc43105af2228925e6f4ed903
wei chen <weichen9083@gmail.com>
2020-12-02 04:23:42 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -46,9 +46,9 @@
       
       def self.new_from_filename(name)
         splited_name = name.split("-")
-        x = self.find_or_create_by_name(splited_name[0])
-        x.artist_name = splited_name[1].slice("mp3")
-        x
+        x = self.create_by_name(splited_name[0])
+        x.artist_name = splited_name[1].delete
+        
       end 
       
       
